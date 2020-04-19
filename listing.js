7bf1661aadbcb39febb88b54ee8b06e5eabb0abc
hm <milstowers@gmail.com>
2020-04-19 18:32:15 +0100

    added

---
+++ listing.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+
+/*
+- Color, category and price range filters must update the list of products in real time
+
+- Products can be sorted three ways: Default, Price: highest first, Price: lowest first. Selecting a sort option must update the list of products in real time
+
+- Clicking the "Add to basket" button will update the cart icon in the top right of the page to show the number of products in the cart
+
+- Products in the cart are stored and remembered by the browser using localStorage
+*/
