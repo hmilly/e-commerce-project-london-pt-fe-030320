4611cdb22bdc763db4d912d1d61b32176070e359
Holly <milstowers@gmail.com>
2020-04-29 17:00:12 +0100

    changed

--- listing.js
+++ listing.js
@@ -49,10 +49,6 @@ mediaAngle.addEventListener("click", () => {
   }
 });
 
-<<<<<<< HEAD
-=======
-
->>>>>>> f6f5b0ca8d73870b44842bc496003bbdf6a3a0b7
 // colourDiv functionality //
 let coloursDiv = document.querySelector(".colours");
 let angle = document.querySelector(".down");
@@ -82,13 +78,11 @@ category.addEventListener("click", () => {
   }
 })
 
-
 // working here on how to display gallery items selected in the listings (type)
 let allCheckboxes = document.querySelectorAll("input[type=checkbox]");
 let allCheckboxesArr = [];
 allCheckboxes.forEach(box => allCheckboxesArr.push(box))
 
-<<<<<<< HEAD
 let catArr = allCheckboxesArr.slice(11, 15)
 
 let secondPitemFromImg = document.querySelectorAll(".name-type p:nth-child(2)")
@@ -124,61 +118,7 @@ secondPitemFromImg.forEach(tick => {
   })
 })
 
-=======
-// working here on how to display gallery items selected in the listings (colour/price/type)
-let allCheckboxes = document.querySelectorAll("input[type=checkbox]");
 
-
-let allCheckboxesArr = [];
-allCheckboxes.forEach(box => allCheckboxesArr.push(box))
-
-let colourArr = allCheckboxesArr.slice(0, 11)
-let catArr = allCheckboxesArr.slice(11, 15)
-
-let item = document.querySelectorAll(".name-type p:nth-child(2)")
->>>>>>> f6f5b0ca8d73870b44842bc496003bbdf6a3a0b7
-
-item.forEach(tick => {
-  catArr[0].addEventListener("change", () => {
-    console.log(tick)
-    if (catArr[0].checked === true && tick.innerText == "CHAIR") {
-      tick.parentElement.parentElement.parentElement.style.display = "none"
-    } else {
-      tick.parentElement.parentElement.parentElement.style.display = "block"
-    }
-  })
-
-<<<<<<< HEAD
-=======
-  catArr[1].addEventListener("change", () => {
-    if (catArr[1].checked === true && tick.innerText == "TABLE") {
-      tick.parentElement.parentElement.parentElement.style.display = "none"
-    } else {
-      tick.parentElement.parentElement.parentElement.style.display = "block"
-    }
-  })
-
-  catArr[2].addEventListener("change", () => {
-    if (catArr[2].checked === true && tick.innerText == "BED") {
-      tick.parentElement.parentElement.parentElement.style.display = "none"
-    } else {
-      tick.parentElement.parentElement.parentElement.style.display = "block"
-    }
-  })
-
-  catArr[3].addEventListener("change", () => {
-    if (catArr[3].checked === true && tick.innerText == "SOFA") {
-      tick.parentElement.parentElement.parentElement.style.display = "none"
-    } else {
-      tick.parentElement.parentElement.parentElement.style.display = "block"
-    }
-  })
-
-})
-
-
-
->>>>>>> f6f5b0ca8d73870b44842bc496003bbdf6a3a0b7
 
 
 
@@ -199,76 +139,12 @@ high.addEventListener("change", (h) => {
 
   if (price.forEach(i => i >= h)) {
     console.log(i)
-<<<<<<< HEAD
 
   }
 })
 
 
-=======
->>>>>>> f6f5b0ca8d73870b44842bc496003bbdf6a3a0b7
 
-  }
-})
-
-
-// all gallery items (20)
-let list_item = [];
-let list_items = document.getElementsByClassName("gallery")
-for (let i = 0; i < list_items.length; i++) {
-  list_item.push(list_items[i])
-}
-// gallery div
-let list_element = document.getElementsByClassName("img-div")
-
-// page number )(4 divs)
-let pageDivs = []
-let pageNum = document.getElementsByClassName("page")
-for (let i = 0; i < pageNum.length; i++) {
-  pageDivs.push(pageNum[i])
-}
-
-let currentP = 1;
-let itemNums = 6;
-
-
-function displayList(items, wrapper, itemspp, page) {
-  wrapper.innerText = "";
-  page--;
-
-  //        20
-  let start = itemspp * page;
-  let end = start + itemspp;
-  let pagi = items.slice(start, end)
-
-  for (let i = 0; i < pagi.length; i++) {
-    let item = pagi[i]
-  }
-}
-
-//             (20,       image_div,      6,        4)
-displayList(list_item, list_element, itemNums, currentP);
-
-
-
-
-/*
-page = 1;
-quantity = 6
- 
-have a startIndex = pageNumber * quantityPerPage
-2. have renderProducts function wich takes an array of products and render them
-3. every time you switch a page you update startIndex , and call renderProducts 
-with spliced array [startIndex, startIndex + quantityPerPage
- 
- 
- 
- 
-let renderProd = (listIarr, )
- 
- 
- 
-*/
 
 // item splits to 6 over 4 pages but doesn't display properLy. No idea how to do this.
 let list_item = [];
@@ -279,7 +155,6 @@ for (let i = 0; i < galleryImage.length; i++) {
 
 let list_element = document.getElementsByClassName("img-div")
 
-
 let pageDivs = []
 let pageNum = document.getElementsByClassName("page")
 for (let i = 0; i < pageNum.length; i++) {
@@ -288,7 +163,6 @@ for (let i = 0; i < pageNum.length; i++) {
 
 let currentP = 1;
 let itemNums = 6;
-
 
 function displayList(items, wrapper, itemspp, page) {
   wrapper.innerText = "";
@@ -306,7 +180,6 @@ function displayList(items, wrapper, itemspp, page) {
 
 //             (20,       image_div,      6,        4)
 displayList(list_item, list_element, itemNums, currentP);
-
 
 
 
@@ -339,7 +212,6 @@ for (let i in PRODUCTS) {
   comfort_level.push(PRODUCTS[i].comfort_level)
 }
 
-
 let cul = [];
 colors.forEach(item => item.forEach(x => cul.push(x)))
 let unique = [...new Set(cul)];
@@ -347,15 +219,12 @@ let unique = [...new Set(cul)];
 let unique2 = [...new Set(type)];
 
 
-<<<<<<< HEAD
-
 
 
 // working here on how to display gallery items selected in the listings (type)
 let colourArr = allCheckboxesArr.slice(0, 11)
 
 console.log(colors)
-
 
 galleryImage.forEach(tick => {
 
@@ -366,7 +235,6 @@ galleryImage.forEach(tick => {
       tick.style.display = "block"
     }
   })
-
 
 
 
@@ -415,6 +283,5 @@ galleryImage.forEach(tick => {
 
 
 
-=======
->>>>>>> f6f5b0ca8d73870b44842bc496003bbdf6a3a0b7
+
 
